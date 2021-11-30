d0cdaccdf98b6ae8448ad8824ce0eca0f34ed9eb
orbis-dev <anvasconcellos@gmail.com>
2021-11-30 01:38:46 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.10:3000',
+  baseURL: process.env.REACT_APP_API_URL,
+  //'http://192.168.0.10:3000',
 });
 
 export default api;
