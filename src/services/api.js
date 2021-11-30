f68743a63b2c68c2202860c74288dd892f4c5d53
orbis-dev <anvasconcellos@gmail.com>
2021-11-30 04:47:18 -0300

    token fix?

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
   //baseURL: "http://192.168.0.10:3000",
-  baseURL: "http://localhost:3000",
+  //baseURL: "http://localhost:3000",
 });
 
 export default api;
