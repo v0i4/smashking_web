7b220d66d594a0782ab8e3c336515c6099e693be
orbis-dev <anvasconcellos@gmail.com>
2021-11-30 05:05:07 -0300

    token fix?

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   //baseURL: process.env.REACT_APP_API_URL,
   //baseURL: "http://192.168.0.10:3000",
-  baseURL: "https://smashking-web.herokuapp.com",
+  baseURL: "http://127.0.0.1:3000",
 });
 
 export default api;
