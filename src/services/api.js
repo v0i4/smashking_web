61c8fb541d87dc6d3db860ae2ad952744a637a84
orbis-dev <anvasconcellos@gmail.com>
2021-11-30 02:32:56 -0300

    testes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  //'http://192.168.0.10:3000',
+  //baseURL: "http://192.168.0.10:3000",
 });
 
 export default api;
